1ba6e9b13caa30d94feecd07d1c19b90b732a299
AngelicaValencia <avalencia42@students.tntech.edu>
2017-04-13 08:56:50 -0500

    Update BSTDriver.cpp

--- BSTDriver.cpp
+++ BSTDriver.cpp
@@ -12,7 +12,6 @@
 #include "CD.h"
 using namespace CSC2110;
 #include <iostream>
-
 using namespace std;
 
 class MyApp: public wxApp
@@ -41,12 +40,35 @@ bool MyApp::OnInit()
       bst->insert(cd);
    }
    delete iter;
-
    
+   /*
    //DO THIS
-   //test your tree sort method
-   CD** unsorted_cds = cds->toArray();
-  // CD** sorted_cds = 
+   //display the height of the binary search tree (not minimum height)
+   //display whether the binary search tree is balanced (should not be balanced)
+
+
+
+
+
+
+
+
+   //create a minimum height binary search tree
+   BinarySearchTree<CD>* min_bst = bst->minimize();
+   bst_iter = min_bst->iterator();
+
+   //make sure that an inorder traversal gives sorted order
+   bst_iter->setInorder();  //takes a snapshot of the data
+   while(bst_iter->hasNext())
+   {
+      CD* cd = bst_iter->next();
+      cd->displayCD();
+   }
+   //delete bst_iter;
+
+   //DO THIS
+   //display the height of the binary search tree (should be minimum height)
+   //display whether the binary search tree is balanced (should be balanced)
 
 
 
@@ -56,7 +78,36 @@ bool MyApp::OnInit()
 
 
 
-   delete cds;
+   //create a complete binary search tree
+   BinarySearchTree<CD>* complete_bst = bst->minimizeComplete();
+   delete bst;
+
+   //make sure that an inorder traversal gives sorted order
+   bst_iter = complete_bst->iterator();
+   bst_iter->setInorder();  //takes a snapshot of the data
+   while(bst_iter->hasNext())
+   {
+      CD* cd = bst_iter->next();
+      cd->displayCD();
+   }
+ //  delete bst_iter;
+
+   //DO THIS
+   //display the height of the complete binary search tree (should be minimum height)
+   //display whether the binary search tree is balanced (should be balanced)
+
+
+
+
+
+
+
+
+
+   delete complete_bst;
+
+  */
+   //delete cds;
    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
    frame = new wxFrame((wxFrame *)NULL, -1,  wxT("AVL Tree"), wxPoint(500,500), wxSize(1100,600));
  
@@ -67,6 +118,5 @@ bool MyApp::OnInit()
    frame->SetAutoLayout(true);
  
    frame->Show();
-   //return true;
-   return 0;
+   return true;
 }
